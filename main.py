d8da93916b3bbb6f0f1a27c58e120b752c3e640d
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2022-01-19 10:35:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def send_image(bot, update):
     bot.send_photo(chat_id=chat_id, photo=open('viz.png', 'rb'))
 
 def main():
-    updater = Updater('YOUR_TOKEN')
+    updater = Updater('5066901644:AAFmrpsXSvXjf2aXWC_gZQAr2CNj5ssyLkg')
     updater.job_queue.run_daily(send_image, time=datetime.datetime.strptime('9:00AM', '%I:%M%p').time(), days=(0,1,2,3,4,5,6))
     updater.start_polling()
     updater.idle()
